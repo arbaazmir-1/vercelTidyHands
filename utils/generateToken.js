47c18e0b4ee7c14ded19fa98a216e84dbcf51e7e
Abdullah Ibn Shahin <arbaazmir@icloud.com>
2022-12-18 16:55:14 +0400

    token generation error prevention added

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,9 @@
 const jwt = require("jsonwebtoken");
 export const generateToken = (id) => {
+  if (!id) {
+    throw new Error("No id provided");
+  }
+
   return jwt.sign(
     { id },
     process.env.JWT_SECRET || import.meta.env.VITE_TEST_JWT_SECRET,
--- utils/generatetoken.test.js
+++ utils/generatetoken.test.js
@@ -1,8 +1,20 @@
 import { it, expect } from "vitest";
 import { generateToken } from "./generateToken.js";
+import jwt from "jsonwebtoken";
 
 it("generates a token", () => {
   const id = "639a28ee55bd66ad6a97337f";
   const token = generateToken(id);
   expect(token).toMatch(/^[\w-]+\.[\w-]+\.[\w-]+$/);
+});
+
+it("generates a token with the correct id", () => {
+  const id = "639a28ee55bd66ad6a973";
+  const token = generateToken(id);
+  const decoded = jwt.decode(token);
+  expect(decoded.id).toBe(id);
+});
+
+it("throws an error if no id is provided", () => {
+  expect(() => generateToken()).toThrow();
 });
